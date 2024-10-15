8efac17f0355db908b8a61ef96a39cffd6761556
Joelgs13 <050213jgonzalez@gmail.com>
2024-10-15 12:03:06 +0200

    UD2.ACT1-3.intento1.inicial
    act1 done

--- UD2/ACT1-3/ACT1_3/src/Act_1/Actividad1.java
+++ UD2/ACT1-3/ACT1_3/src/Act_1/Actividad1.java
@@ -1,6 +1,15 @@
 package Act_1;
 
+/**
+ * The Class Actividad1.
+ */
 public class Actividad1 {
+	
+	/**
+	 * The main method, creates 2 Hilo objects and runs this ones, showing a final comment of the program ending
+	 *
+	 * @param arg the arguments
+	 */
 	public static void main(String arg[]) {
 		Hilo h1=new Hilo();
 		Hilo h2=new Hilo();
--- UD2/ACT1-3/ACT1_3/src/Act_1/Hilo.java
+++ UD2/ACT1-3/ACT1_3/src/Act_1/Hilo.java
@@ -1,6 +1,13 @@
 package Act_1;
 
+/**
+ * The Class Hilo, that represents the Primero and Segundo classes all in 1. Is the class that haves the method to run the Threads
+ */
 public class Hilo extends Thread{
+	
+	/**
+	 * The method to run the threads maded. It uses the getName method of the interface Thread to name each of the threads respectively.
+	 */
 	public void run(){
 		for (int i=1;i<20;i++)
 		System.out.println(this.getName()+" " + i);
