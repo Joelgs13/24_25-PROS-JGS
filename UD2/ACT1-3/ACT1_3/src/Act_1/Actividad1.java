10dcdb55ba2acf953801df11ec81a1e4a1bf63ab
Joelgs13 <050213jgonzalez@gmail.com>
2024-10-15 11:47:24 +0200

    UD2.ACT1.intento1.code done

--- UD2/ACT1-3/ACT1_3/src/Act_1/Actividad1.java
+++ UD2/ACT1-3/ACT1_3/src/Act_1/Actividad1.java
@@ -1,5 +1,11 @@
 package Act_1;
 
 public class Actividad1 {
-
+	public static void main(String arg[]) {
+		Hilo h1=new Hilo();
+		Hilo h2=new Hilo();
+		h1.start();
+		h2.start();
+		System.out.print("Fin programa");
+	}
 }
--- UD2/ACT1-3/ACT1_3/src/Act_1/Hilo.java
+++ UD2/ACT1-3/ACT1_3/src/Act_1/Hilo.java
@@ -1,5 +1,8 @@
 package Act_1;
 
 public class Hilo extends Thread{
-
+	public void run(){
+		for (int i=1;i<20;i++)
+		System.out.println(this.getName()+" " + i);
+	}
 }
