49c7570367f57e66766dd3692a9e7f71c842f6a8
Joelgs13 <050213jgonzalez@gmail.com>
2024-10-22 12:11:33 +0200

    UD2.ACT10.intento1.all done, still need to document

--- UD2/ACT10/ACT10/src/act_10/Actividad10.java
+++ UD2/ACT10/ACT10/src/act_10/Actividad10.java
@@ -1,5 +1,20 @@
+
+
 package act_10;
 
 public class Actividad10 {
-
+	public static void main(String[] args) {
+		Thread main = new Thread();
+		Hilo thread0 = new Hilo("Hilo-prioridad 3");
+		Hilo thread1 = new Hilo("Hilo-prioridad 7");
+		thread0.setPriority(3);
+		thread1.setPriority(7);
+		main.setName("main");
+		System.out.println(main.getName()+" tiene la prioridad "+main.getPriority());
+		System.out.println(thread0.getName()+" tiene la prioridad "+thread0.getPriority());
+		System.out.println(thread1.getName()+" tiene la prioridad "+thread1.getPriority());
+		thread1.start();
+		thread0.start();
+		System.out.println("final programa");
+	}
 }
--- UD2/ACT10/ACT10/src/act_10/Hilo.java
+++ UD2/ACT10/ACT10/src/act_10/Hilo.java
@@ -1,5 +1,11 @@
 package act_10;
 
-public class Hilo {
-
+public class Hilo extends Thread{
+	private String message;
+	public Hilo(String mensaje) {
+		this.message=mensaje;
+	}
+	public void run() {
+		System.out.println("Ejecutando "+this.message);
+	}
 }
