3c18c23b87c18803eda51a987e1b4a0033a66bae
Joelgs13 <050213jgonzalez@gmail.com>
2024-10-29 11:41:44 +0100

    UD2.ACT14.intento1.definitivo

--- UD2/ACT14/ACT14/src/act_14/Activity14.java
+++ UD2/ACT14/ACT14/src/act_14/Activity14.java
@@ -1,11 +1,22 @@
 package act_14;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Activity14.
+ */
 public class Activity14 extends Thread { 
+	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String args[]) {
 		Recurso a = new Recurso(); 
 		Recurso b = new Recurso(); 
-		Hilo h1 = new Hilo(a, b, "uno"); 
-		Hilo h2 = new Hilo(b, a, "dos"); 
+		Hilo h1 = new Hilo(a, b, "uno");
+		//change the order of the atributes!!
+		Hilo h2 = new Hilo(a, b, "dos"); 
 		h1.start(); 
 		h2.start(); 
 	}
--- UD2/ACT14/ACT14/src/act_14/Hilo.java
+++ UD2/ACT14/ACT14/src/act_14/Hilo.java
@@ -1,13 +1,33 @@
 package act_14;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Hilo.
+ */
 class Hilo extends Thread { 
+	
+	/** The a. */
 	Recurso a; 
+	
+	/** The b. */
 	Recurso b; 
+	
+	/**
+	 * Instantiates a new hilo.
+	 *
+	 * @param a the a
+	 * @param b the b
+	 * @param nombre the nombre
+	 */
 	public Hilo(Recurso a, Recurso b,String nombre) { 
 		super(nombre); 
 		this.a = a; 
 		this.b = b; 
 	} 
+	
+	/**
+	 * Run.
+	 */
 	public void run(){ 
 		System.out.println("Hilo " + this.getName() + " comienza"); 
 		synchronized(a) { 
