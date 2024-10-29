ffe4a4ca990e646756580c75c4e0eb6d0a9054b5
Joelgs13 <050213jgonzalez@gmail.com>
2024-10-29 12:01:25 +0100

    UD2.ACT15-16.intento1.inicial

---
+++ UD2/ACT15-16/ACT15_16/src/Act_15/Contador.java
@@ -0,0 +1,19 @@
+package Act_15;
+	class Contador {
+		private int c=0;
+		Contador (int c) { 
+			this.c = c; 
+		}
+		
+		public void incrementa() {
+			c=c+1;
+		}
+		
+		public void decrementa() {
+			c=c-1;
+		}
+		
+		public int getValor() {
+			return c; 
+		}
+	}
